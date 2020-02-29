bc3b7996317ba57136e283a199535c342fd2a547
g-gibosky <32047751+g-gibosky@users.noreply.github.com>
2020-02-29 16:48:31 -0300

    Gitignore update

--- github_class.py
+++ github_class.py
@@ -6,15 +6,13 @@ import os
 def main():
 	# First create a Github instance:
 	# using username and password
-	g = Github("g-gibosky", "Systr@setop2019")
+	g = Github("ouranus", "balada66")
 
 	# or using an access token
-	g = Github("access_token")
+	g = Github("ac44d1f39fea30e606dce90b3532ce6102a7e470")
 
 	# Github Enterprise with custom hostname
-	g = Github(base_url="https://github.com/g-gibosky/Trello-Ticket/api/v3", login_or_token="access_token")
-
-	pprint(g)
+	g = Github(base_url="https://github.com/ouranus/TrelloTicket/api/v3", login_or_token="username")
 
 	# Then play with your Github objects:
 	for repo in g.get_user().get_repos():
